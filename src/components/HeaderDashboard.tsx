9d3cadda87b722e6cde71613714a6a7ef20c2c0d
fahmi277 <fahmi.sangengon@gmail.com>
2025-08-24 16:35:17 +0700

    remove assets

--- src/components/HeaderDashboard.tsx
+++ src/components/HeaderDashboard.tsx
@@ -15,7 +15,6 @@ const HeaderDashboard: React.FC<HeaderDashboardProps> = ({
     <>
       <div className="h-[74px] w-full flex bg-white items-center justify-between px-4">
         <img
-          // src="../src/assets/logo.png"
           src="logo.png"
           alt="Videobelajar Logo"
           className="h-[42px] w-[152px] ml-[24px] md:h-[56px] md:w-[237px] md:ml-[120px]"
